c1fbd4f3110c8dd2c6bd6494ee25f103e48fbc4a
Konstantin.Myakshin <koc-dp@yandex.ru>
2013-11-08 18:48:11 +0200

    Pagerfanta adapter.

---
+++ Pagerfanda/Adapter/SphinxyQbAdapter.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Brouzie\Sphinxy\Pagerfanta\Adapter;
+
+use Brouzie\Sphinxy\Query\ResultSet;
+use Brouzie\Sphinxy\QueryBuilder;
+use Pagerfanta\Adapter\AdapterInterface;
+
+/**
+ * Sphinxy Pagerfanta Adapter
+ *
+ * @author Konstantin.Myakshin <koc-dp@yandex.ru>
+ */
+class SphinxyQbAdapter implements AdapterInterface
+{
+    /**
+     * @var QueryBuilder
+     */
+    protected $qb;
+
+    /**
+     * @var ResultSet|null
+     */
+    protected $previousResultSet;
+
+    public function __construct(QueryBuilder $qb)
+    {
+        $this->qb = $qb;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getNbResults()
+    {
+        if (null !== $this->previousResultSet) {
+            return $this->previousResultSet->getTotalCount();
+        }
+
+        $this->getSlice(0, 1);
+
+        return $this->previousResultSet->getTotalCount();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSlice($offset, $length)
+    {
+        $this->previousResultSet = $this
+            ->qb
+            ->setMaxResults($length)
+            ->setFirstResult($offset)
+            ->getResult();
+
+        return $this->previousResultSet->getIterator();
+    }
+}
